85c091bcfb1e614126d331b9c635194df49f41dd
fexh <90156014+fexh10@users.noreply.github.com>
2022-12-10 11:34:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import find_packages, setup
-
+    
 setup(
     name="aw-cli",
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     py_modules=['aw-cli'],
 >>>>>>> b274f43 (Update setup.py)
+=======
+    packages=find_packages(include=["aw-cli"]),
+>>>>>>> d6df4ff (Update setup.py)
     version="1.0",
     python_requires=">3.10",
     description="guarda anime dal terminale e molto altro!",
@@ -21,11 +25,16 @@ setup(
         "hpcomt",
     ],
 <<<<<<< HEAD
+<<<<<<< HEAD
      entry_points="[console_scripts]aw-cli=aw-cli:main",
      py_modules=['aw-cli'],
 
 =======
     entry_points="[console_scripts]aw-cli=aw-cli:main",
 >>>>>>> b274f43 (Update setup.py)
+=======
+    entry_points="[console_scripts]\aw-cli=aw_cli.run_aw_cli:main",
+
+>>>>>>> d6df4ff (Update setup.py)
 )
 
