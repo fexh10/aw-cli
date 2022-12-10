d3cb7bce828d6379be99d287079506d0fe5cf19c
fexh <90156014+fexh10@users.noreply.github.com>
2022-12-10 10:36:37 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="aw-cli",
+    packages=find_packages(include=["aw-_cli"]),
+    version="1.0",
+    python_requires=">3.10",
+    description="guarda anime dal terminale e molto altro!",
+    author="fexh10",
+    url="https://github.com/fexh10/aw-cli",
+    license="GPL-3.0",
+    install_requires=[
+        "bs4",
+        "requests",
+        "python-mpv",
+        "lxml",
+        "pySmartDL",
+        "hpcomt",
+    ],
+)
