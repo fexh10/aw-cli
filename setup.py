b3b3d50fec3b63ee6e8183d14c08dc79499504a1
fexh <90156014+fexh10@users.noreply.github.com>
2022-12-11 16:34:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages, setup
 import os
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="aw-cli",
     packages=find_packages(include=["awcli"]),
     version="1.1",
     python_requires=">3.10",
     description="guarda anime dal terminale e molto altro!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="fexh10",
     url="https://github.com/fexh10/aw-cli",
     license="GPL-3.0",
