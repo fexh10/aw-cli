3cc28ea6220da056f237a878c7d0b49b87f54221
fexh10 <effehgi@gmail.com>
2023-02-13 08:23:58 +0100

    v1.4.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if os.name == 'nt':
 setup(
     name="aw-cli",
     packages=find_packages(include=["awcli"]),
-    version="1.4",
+    version="1.4.1",
     python_requires=">3.10",
     description="guarda anime dal terminale e molto altro!",
     long_description=long_description,
