99569a63723ce9d91fbeff8424e5ff3b2f502dd7
fexh10 <effehgi@gmail.com>
2024-05-06 17:24:24 +0200

    v1.8.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ installRequires = [
 setup(
     name="aw-cli",
     packages=find_packages(include=["awcli"]),
-    version="1.8",
+    version="1.8.1",
     python_requires=">3.10",
     description="guarda anime dal terminale e molto altro!",
     long_description=long_description,
