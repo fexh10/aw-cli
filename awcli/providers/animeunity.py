fa48a9eabaf8be18edc0abc8dc15ad275ade890b
axtrat <valentivaledx+github@gmail.com>
2025-08-21 02:59:23 +0200

    fix: corretto l'errore per gli anime con più di 119 episodi, e altri bug di tipo

--- awcli/providers/animeunity.py
+++ awcli/providers/animeunity.py
@@ -82,30 +82,45 @@ class Animeunity(Provider):
                 continue
             if filter == "s" and info["Audio"] == "Italiano":
                 continue
-            anime = Anime(title, result['id'], int(data['number']))
+            anime = Anime(title, result['id'], str(data['number']))
             anime._set_info(anilist_id, info)
             animes.append(anime)
         return animes
         
     def episodes(self, anime: Anime):
-        search_url = f"{self._url}/info_api/{anime.url}/1"
-        self._get_token()  # Assicura che i token/cookie siano aggiornati
-        try:
-            response = requests.get(
-            url=search_url,
-            params={
-                "start_range": 0,
-                "end_range": max(int(anime.info["Episodi"]), int(anime.last_ep))
-            },
-            cookies=self._cookies,
-            headers=self._headers,
-            timeout=10
-            )
-            response.raise_for_status()
-        except Exception as e:
-            ut.my_print(f"Errore nella richiesta degli episodi: {e}", color="rosso")
-            exit()
-        episodi = {episode['number']: episode['id'] for episode in response.json()['episodes']}
+        episode_count = max(int(anime.info["Episodi"] if anime.info["Episodi"].isdigit() else int(anime.curr_ep)), int(anime.last_ep))
+        if episode_count < 1:
+            self.info_anime(anime)
+            episode_count = int(anime.info["Episodi"])
+        episodi = {}
+        start_range = 1
+        # Fetch episodes in chunks
+        while start_range <= episode_count:
+            end_range = min(start_range + 119, episode_count)
+            search_url = f"{self._url}/info_api/{anime.url}/1"
+            self._get_token()  # Assicura che i token/cookie siano aggiornati
+            try:
+                response = requests.get(
+                url=search_url,
+                params={
+                    "start_range": start_range,
+                    "end_range": end_range
+                },
+                cookies=self._cookies,
+                headers=self._headers,
+                timeout=10
+                )
+                response.raise_for_status()
+            except requests.exceptions.HTTPError:
+                ut.my_print("Il link è stato cambiato", color="rosso", end="\n")
+                anime.url = self.search(anime.name)[0].url
+                return self.episodes(anime)  # Retry with the new URL
+            except Exception as e:
+                ut.my_print(f"Errore nella richiesta degli episodi: {e}", color="rosso")
+                exit()
+            #episodi = {episode['number']: episode['id'] for episode in response.json()['episodes']}
+            episodi.update({episode['number']: episode['id'] for episode in response.json()['episodes']})
+            start_range = end_range + 1
         anime._set_episodes(episodi)
 
     def episode_link(self, episode: Episode) -> str:
@@ -148,6 +163,7 @@ class Animeunity(Provider):
             ut.my_print(f"Errore nella richiesta di info anime: {e}", color="rosso")
             exit()
         data = response.json()
+        anime.info["Episodi"] = str(data['episodes_count']) 
         anime.info["Genere"] = ', '.join(data['genres'])
         anime.info["Correlati"] = data['related']
         
@@ -163,7 +179,7 @@ class Animeunity(Provider):
             "Studio": data['studio'],
             "Voto": data['score'],
             "Durata": f"{data['episodes_length']} min",
-            "Episodi": data['episodes_count'],
+            "Episodi": str(data['episodes_count']),
             "Stato": "0" if data['status'] == "In Corso" else "1" if data['status'] == "Terminato" else "2",
             "Visualizzazioni": data['visite'],
             "Trama": data['plot'],
